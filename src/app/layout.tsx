fcbb749cd026492ce787c65bf20e4f795631dc34
DOUMBA Arsel <amakita124@gmail.com>
2025-05-30 18:29:19 +0100

    Navebaré

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MobileMenu from "@/components/utilisateur/MobileMenu"
+import Navbar from "@/components/utilisateur/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > <MobileMenu />
+    <html lang="fr">
+      <body >
+        <Navbar />
+        <main>
         {children}
+        </main>
       </body>
     </html>
   );
