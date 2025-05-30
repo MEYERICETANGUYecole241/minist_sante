503f8c40b71ac11ac77515dfb2321ceab2a5d3ac
ICE Tanguy <icetanguy065@gmail.com>
2025-05-30 16:20:46 +0100

    commit 2

---
+++ src/components/sections/NewSections.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewSections() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/sections/ServicesSections.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServicesSections() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
