876a674b54b4ea2b11e680bc0cdf30651bc1fb88
Christian-Assame <christianassame0@gmail.com>
2025-05-31 09:38:59 +0100

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "@/components/utilisateur/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,9 +27,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+
       >
         {children}
-      </body>
+        <Footer />
+        </body>
     </html>
   );
 }
--- src/components/utilisateur/MobileMenu.tsx
+++ src/components/utilisateur/MobileMenu.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function MobileMenu() {
   return (
     <div>
+      <h1>
+        gft
+      </h1>
       
     </div>
   )
